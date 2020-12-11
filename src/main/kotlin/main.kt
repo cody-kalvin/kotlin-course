d7505900a03c784ddfb27d4692c52d61077b2802
Kalvin dela Raga <kalvin_delaraga@ligph.com>
2020-12-11 13:27:45 +0800

    add comments

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,8 @@
+/**
+ * This is a multiline comment.
+ * This has two to be exact.
+ */
 fun main() {
+    // This is a single line comment
     println("You're expecting a \"Hello\". But it was me, Dio.")
 }
